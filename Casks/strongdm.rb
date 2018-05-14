684cb8f2ea11f084e2b3436e079ee00ec130eb91
Steve Huff <steve.huff@asics.com>
2018-05-14 11:46:52 -0400

    feat: strongDM 0.4.21

--- Casks/strongdm.rb
+++ Casks/strongdm.rb
@@ -1,6 +1,6 @@
 cask 'strongdm' do
-  version '0.4.14'
-  sha256 '2bc8f04aa632e3816dfabeffa16b9ea77f677a295e26951a4de9254d253dfca6'
+  version '0.4.21'
+  sha256 '990e9ff6b13a4fd04d19fbdc6100c158c9309ccc32cc864b0be58e27218e4986'
 
   # sdm-releases-production.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://sdm-releases-production.s3.amazonaws.com/nightly/darwin/SDM-#{version}.dmg"
